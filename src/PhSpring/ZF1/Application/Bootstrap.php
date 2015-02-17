bc4846145ff16efd3cb53ec6d7abd6578d29b3d1
PhSpring <info@phspring.nl>
2015-02-17 02:17:49 +0100

    BugFix

--- src/PhSpring/ZF1/Application/Bootstrap.php
+++ src/PhSpring/ZF1/Application/Bootstrap.php
@@ -9,10 +9,14 @@
 namespace PhSpring\ZF1\Application;
 
 use PhSpring\Engine\InvokerConfig;
+use PhSpring\ZF1\Application\Resource\ServiceLoader;
+use PhSpring\ZF1\Controller\Dispatcher;
 use PhSpring\ZF1\Controller\Request;
 use PhSpring\ZF1\Controller\Response;
+use Zend_Config;
 use Zend_Controller_Action_HelperBroker;
 use Zend_Controller_Front;
+use Zend_Registry;
 
 /**
  * Description of Bootstrap
@@ -20,6 +24,15 @@ use Zend_Controller_Front;
  * @author lobiferi
  */
 class Bootstrap extends \Zend_Application_Bootstrap_Bootstrap {
+
+    public function __construct($application) {
+        parent::__construct($application);
+        Zend_Controller_Front::getInstance()->setDispatcher(new Dispatcher());
+        $this->config = new Zend_Config($this->getOptions());
+        Zend_Registry::set('Zend_Config', $this->config);
+        Zend_Registry::set('config', $this->config);
+        new ServiceLoader($this->getOptions());
+    }
 
     protected function _initInvokerConfig() {
         $request = new Request();
